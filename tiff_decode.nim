963f6767b8b64058ea10e7cbc5b65bbc6c3e4f7c
James Guevara <guevara.james@gmail.com>
2021-07-27 01:13:03 -0700

    can now save a jpeg image! but its dimensions are flipped, fix that and try outputting it in a window

--- tiff_decode.nim
+++ tiff_decode.nim
@@ -69,9 +69,112 @@ proc process_tiff(): seq[image_file_directory] =
 var ifds = process_tiff()
 echo ifds
 
+var ifd = ifds[0]
+echo "\n\n\n\n\n\n"
+var ifd_entries = ifd.ifd_entries
+
+
+
+
+
 
 # Locate ifd_entries with certain tags in them
+# Which tags? (and corresponding tag numbers)
+# ImageWidth (256), ImageLength (257), StripOffsets (273), RowsPerStrip (278), StripByteCounts (279)
+# Get the pixel values
+# Run a mean function on them
+var imageWidth: int32
+var imageLength: int32
+var stripOffsets: int32
+var rowsPerStrip: int32
+var stripByteCounts: int32
+var bitsPerSample: int32
+for ifd_entry in ifd_entries:
+  var value = ifd_entry.value_file_offset
+  var tag = ifd_entry.tag
+  if tag == 256:
+    imageWidth = value
+  elif tag == 257:
+    imageLength = value
+  elif tag == 273:
+    stripOffsets = value
+  elif tag == 278:
+    rowsPerStrip = value
+  elif tag == 279:
+    stripByteCounts = value
+  elif tag == 258:
+    bitsPerSample = value
+
+echo "imageWidth: ", imageWidth
+echo "imageLength: ", imageLength
+echo "stripOffsets: ", stripOffsets
+echo "rowsPerStrip: ", rowsPerStrip
+echo "stripByteCounts: ", stripByteCounts
+echo "bitsPerSample: ", bitsPerSample
+
+# (For my test image:)
+# imageWidth: 375
+# imageLength: 242
+# stripOffsets: 8
+# rowsPerStrip: 242
+# stripByteCounts: 90750
+
+# go to stripOffsets first, then get all pixels (not split up this time, can be split up later on)
+# annoying things to take care of later: endianness (is it annoying?), strips that aren't contiguous 
 
 
+import arraymancer
+import sequtils
 
+# var numPixels = imageLength*imageWidth.int
+# var a = toSeq(1..numPixels).toTensor().reshape(imageLength.int, imageWidth.int)
+
+# echo a[0,0]
+
+# get pixels (assuming bitsPerSample is 8 which it is in this case, need to handle other cases)
+var s = newSeq[seq[uint8]](imageLength)
+for i in 0 ..< imageLength:
+  s[i].newSeq(imageWidth)
+
+setPosition(stream, stripOffsets)
+
+for i in countup(0, imageLength - 1):
+  for j in countup(0, imageWidth - 1):
+    s[i][j] = stream.readUint8()
+
+# echo s
+
+# for i in countup(1, stripByteCounts):
+#   echo i
+
+
+import nigui
+
+
+app.init()
+var window = newWindow()
+window.width = imageWidth
+window.height = imageLength
+
+var image = newImage()
+image.resize(imageLength, imageWidth)
+
+for i in countup(0, imageLength - 1):
+  for j in countup(0, imageWidth - 1):
+    var val = s[i][j]
+    # echo val
+    image.canvas.setPixel(i, j, rgb(val, val, val))
+
+var image2 = newImage()
+image2.resize(2, 2)
+# Creates a new bitmap
+image2.canvas.setPixel(0, 0, rgb(255, 0, 0))
+image2.canvas.setPixel(0, 1, rgb(255, 0, 0))
+image2.canvas.setPixel(1, 1, rgb(0, 255, 0))
+image2.canvas.setPixel(1, 0, rgb(0, 0, 255))
+
+saveToJpegFile(image, "testing_E.jpg")
+
+window.show()
+app.run()
 
